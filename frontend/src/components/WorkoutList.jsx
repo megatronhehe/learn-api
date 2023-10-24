e11bbf535aa38a73628e96090404cb3321106fc6
Megatronhehee <satyamahendra09@gmail.com>
2023-10-24 20:10:40 +0700

    fix: better list design

--- frontend/src/components/WorkoutList.jsx
+++ frontend/src/components/WorkoutList.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-import { TbAnalyze, TbWeight } from "react-icons/tb";
+import { TbAnalyze, TbWeight, TbTrashX, TbPencil } from "react-icons/tb";
 
 export default function WorkoutList({ workout }) {
 	const { load, reps, title } = workout;
 
 	return (
-		<li className="px-4 py-2 border rounded-xl flex flex-col">
-			<h2 className="w-2/3">{title}</h2>
-			<ul className="flex gap-4">
+		<li className="flex items-center px-4 py-2 border rounded-xl">
+			<h2 className="w-2/5">{title}</h2>
+
+			<ul className="flex w-2/5 gap-4">
 				<li className="flex items-center gap-1">
 					<TbWeight />
 					<p>
@@ -20,6 +21,15 @@ export default function WorkoutList({ workout }) {
 					<p>
 						{reps} <span className="text-xs">reps</span>
 					</p>
+				</li>
+			</ul>
+
+			<ul className="flex justify-end w-1/5 gap-1 text-gray-400">
+				<li>
+					<TbPencil className="text-xl" />
+				</li>
+				<li>
+					<TbTrashX className="text-xl" />
 				</li>
 			</ul>
 		</li>
