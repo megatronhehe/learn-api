debbb74f7ebd0c1898f03514ebe32d242270a484
Megatronhehee <satyamahendra09@gmail.com>
2023-10-26 12:39:41 +0700

    feature added: update a workout/excercise

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ app.use(express.json());
 // Enable CORS for all routes
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your React app's origin
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH"
+	);
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	next();
--- frontend/src/components/WorkoutItem.jsx
+++ frontend/src/components/WorkoutItem.jsx
@@ -7,6 +7,8 @@ import {
 	TbLoader2,
 	TbCircle,
 	TbCircleDashed,
+	TbCheck,
+	TbX,
 } from "react-icons/tb";
 
 import WorkoutsContext from "../context/WorkoutsContext";
@@ -16,8 +18,16 @@ export default function WorkoutItem({ workout }) {
 
 	const { setWorkouts } = useContext(WorkoutsContext);
 
+	const [curWorkout, setCurWorkout] = useState({
+		title: title,
+		reps: reps,
+		load: load,
+		sets: sets,
+	});
 	const [isDeleting, setIsDeleting] = useState(false);
+	const [isEditing, setIsEditing] = useState(false);
 	const [errorMsg, setErrorMsg] = useState("");
+	const [enableEdit, setEnableEdit] = useState(false);
 
 	const deleteWorkout = async (id) => {
 		setIsDeleting(true);
@@ -40,14 +50,65 @@ export default function WorkoutItem({ workout }) {
 		}
 	};
 
+	const editWorkout = async (id) => {
+		setIsEditing(true);
+		setErrorMsg("");
+		try {
+			const res = await fetch(`http://localhost:4000/api/workouts/${id}`, {
+				method: "PATCH",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(curWorkout),
+			});
+
+			if (!res.ok) {
+				return setErrorMsg(`${res.status} | ${res.statusText}`);
+			}
+
+			const data = await res.json();
+			setWorkouts((prev) =>
+				prev.map((workout) => (workout._id === data._id ? data : workout))
+			);
+			setEnableEdit(false);
+		} catch (error) {
+			setErrorMsg(error.message);
+		} finally {
+			setIsEditing(false);
+		}
+	};
+
+	const handleEditWorkout = (e) => {
+		const { name, value } = e.target;
+		setCurWorkout((prev) => ({ ...prev, [name]: value }));
+	};
+
 	return (
 		<li className="flex flex-col overflow-hidden border rounded-xl">
 			<div className="flex items-center justify-between p-2">
-				<h2>{title}</h2>
+				<input
+					type="text"
+					name="title"
+					disabled={!enableEdit}
+					onChange={handleEditWorkout}
+					value={curWorkout.title}
+					className="px-2 bg-white"
+				/>
 				<ul className="flex gap-2 text-xl text-gray-400">
+					{enableEdit && (
+						<li>
+							<button
+								onClick={() => editWorkout(_id)}
+								className="duration-200 hover:scale-125"
+							>
+								<TbCheck />
+							</button>
+						</li>
+					)}
 					<li>
-						<button className="duration-200 hover:scale-125">
-							<TbPencil />
+						<button
+							onClick={() => setEnableEdit((prev) => !prev)}
+							className="duration-200 hover:scale-125"
+						>
+							{enableEdit ? <TbX /> : <TbPencil />}
 						</button>
 					</li>
 					<li>
@@ -65,22 +126,51 @@ export default function WorkoutItem({ workout }) {
 					</li>
 				</ul>
 			</div>
-			<div className="py-1.5 px-2 text-sm bg-gray-100">
-				<ul className="flex gap-6">
+			<div className="py-1.5 px-4 text-sm bg-gray-100">
+				<ul className="flex gap-10">
 					<li className="flex items-center gap-1">
 						<TbCircleDashed />
-						{reps}
-						<span className="text-xs">reps</span>
+						<input
+							disabled={!enableEdit}
+							type="number"
+							name="reps"
+							onChange={handleEditWorkout}
+							value={curWorkout.reps}
+							className="w-10 pl-1 rounded-lg"
+						/>
+						{!enableEdit && (
+							<span className="absolute ml-10 text-xs">reps</span>
+						)}
 					</li>
+
 					<li className="flex items-center gap-1">
 						<TbCircle />
-						{sets}
-						<span className="text-xs">sets</span>
+						<input
+							disabled={!enableEdit}
+							type="number"
+							name="sets"
+							onChange={handleEditWorkout}
+							value={curWorkout.sets}
+							className="w-10 pl-1 rounded-lg"
+						/>
+						{!enableEdit && (
+							<span className="absolute ml-10 text-xs">sets</span>
+						)}
 					</li>
+
 					<li className="flex items-center gap-1">
 						<TbWeight />
-						{load}
-						<span className="text-xs">(Kg)</span>
+						<input
+							disabled={!enableEdit}
+							type="number"
+							name="load"
+							onChange={handleEditWorkout}
+							value={curWorkout.load}
+							className="w-10 pl-1 rounded-lg"
+						/>
+						{!enableEdit && (
+							<span className="absolute ml-10 text-xs">(Kg)</span>
+						)}
 					</li>
 				</ul>
 			</div>
